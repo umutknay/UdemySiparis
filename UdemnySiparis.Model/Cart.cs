ab717c12ad86e28e2870e65e1205aa79c54fc2e0
umutknay <68593522+umutknay@users.noreply.github.com>
2022-03-29 17:03:19 +0300

    Update Cart.cs

--- UdemnySiparis.Model/Cart.cs
+++ UdemnySiparis.Model/Cart.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,22 @@ namespace UdemnySiparis.Models
 {
     public class Cart
     {
+        public Cart()
+        {
+            Count = 1;
+        }
+        [Key]
+        public int Id { get; set; }
+        public string AppUserId { get; set; }
+        public int ProductId{ get; set; }
+        public int Count { get; set; }
+        public double Price { get; set; }
+
+        [ForeignKey("AppUserId")]
+        public  AppUser AppUser { get; set; }
+
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
     }
+
 }
